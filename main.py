19bd5c6162d22ae70475bde80d25d522233237ef
Bet Eugene <zheniaz474@gmail.com>
2022-10-23 22:01:34 +0300

    Test commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
+import sys
+import Colors
 
 pygame.init()
+
+screen = pygame.display.set_mode((Colors.WIDTH, Colors.HEIGHT))
+pygame.display.set_caption("Mario Remaster")
+pygame.display.set_icon(pygame.image.load(r"C:\GitRepos\git-KourseGame\images\icon.png"))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+
+    screen.fill(Colors.BLUE)
+    pygame.display.update()
